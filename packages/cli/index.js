e75c7a25c723fe7f68f593d7741eb041aed53bdb
Ilchishin Denis <ilchishin.denis@gmail.com>
2023-02-10 08:00:06 +0800

    chore: update package.json

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -7,10 +7,11 @@ const { start } = require('@neemata/core')
 
 const { positionals, values } = parseArgs({
   options: {
-    config: { type: 'string', short: 'c' },
-    root: { type: 'string', short: 'r' },
+    config: { type: 'string', short: 'c', default: 'neemata.config.js' },
+    root: { type: 'string', short: 'r', default: 'application' },
     scheduler: { type: 'boolean', short: 's' },
     timeout: { type: 'string', short: 't' },
+    dotenv: { type: 'boolean', short: 'e', default: true },
   },
   allowPositionals: true,
   strict: true,
@@ -19,8 +20,10 @@ const { positionals, values } = parseArgs({
 const { config, root, timeout, scheduler: startScheduler } = values
 const [command, ...args] = positionals
 
-const rootPath = resolve(root || 'application')
-const configPath = resolve(config || 'neemata.config.js')
+const rootPath = resolve(root)
+const configPath = resolve(config)
+
+if (dotenv) require('dotenv').config()
 
 start({
   rootPath,

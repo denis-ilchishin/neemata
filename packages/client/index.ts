7eb0ccf9db8047feabd00ab36235f2995f176f0a
Denis Ilchishin <ilchishin.denis@gmail.com>
2023-09-24 16:34:53 +0700

    fix: ts error

--- packages/client/index.ts
+++ packages/client/index.ts
@@ -244,7 +244,6 @@ export const createClient = (options: Options) => {
 
 const createStream = (send: SendWithWs, file: File) => {
   const emitter = new EventEmitter()
-  // @ts-expect-error
   const reader = file.stream().getReader()
   const id = nextStreamId++
   const meta: StreamMeta = {
